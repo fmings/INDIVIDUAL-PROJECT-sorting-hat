558569a8473e77c80373d3f7a91c9b6017bd36ee
fmings <mingsfelicia@gmail.com>
2024-01-29 21:55:27 -0600

    created functioning filters

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 
 console.log("testing the link");
 
-//const houseArray = ["Ravenclaw", "Hufflepuff", "Gryffindor", "Slytherin"]
-//const randomHouse = Math.floor(Math.random()*houseArray.length)
+const houseArray = ["Ravenclaw", "Hufflepuff", "Gryffindor", "Slytherin"];
+
 const dataList = [
   {
   id: 1,
@@ -17,19 +17,6 @@ const renderToDom = (divId, htmlToRender) => {
   selectedDiv.innerHTML = htmlToRender
 }
 
-//function for rendering the welcome card upon start of application
-//const welcomeCardOnDom = () => {
-  //let domString = `
-  //<div class="card" id="welcomeCard">
-  //<div class="card-body">
-    //<h5 class="card-title">Welcome to the Sorting Hat's Sorting House!</h5>
-    //<p class="card-text">Click below to start the sorting process</p>
-    //<a href="#" class="btn btn-primary" id="start-sorting-btn">deligitor prodeas</a>
-  //</div>
-//</div>`;
-
-//renderToDom("#welcome-card-container", domString)
-//}
 
 //function for rendering the object/sorting cards
 const cardsOnDom = (array) => {
@@ -48,13 +35,15 @@ const cardsOnDom = (array) => {
   renderToDom("#card-container",domString)
 }
 
+//variable to hide the form until button click
 const hiddenform = document.querySelector("#form-div").style.display = "none";
 
+//function to show the form once button is clicked
 function showForm() {
   const showForm = document.querySelector("#form-div").style.display = "block"
 }
 
-
+//query and function to make the button work/to show the form
 const entryButtonEl = document.querySelector("#start-sorting-btn");
 
 entryButtonEl.addEventListener("click", () => {
@@ -64,14 +53,26 @@ entryButtonEl.addEventListener("click", () => {
 
 const form = document.querySelector("form")
 
+//() => {
+  //return dataList.map((e) => (dataList.type === "Ravenclaw"))
+//})
 
+//const ravenclawHouse = () => dataList.map((e) => (dataList.house === "Ravenclaw"))
+
+function random() {
+    const randomHouse = Math.floor(Math.random()*houseArray.length);
+    const randomHouseName = houseArray[randomHouse];
+    return randomHouseName
+}
+
+//function to add card and assign new students a house
 const createCard = (e) => {
   e.preventDefault();
 
   const newSortObject = {
     id: dataList.length + 1,
     name: document.querySelector("#name-input").value,
-    house: "slytherin",
+    house: random(),
     nonexpelled: true 
   };
   dataList.push(newSortObject);
@@ -81,6 +82,29 @@ const createCard = (e) => {
 
 form.addEventListener("submit", createCard)
 
+
+
+const houseFilter = (event) => {
+  if(event.target.id.includes('gryffindor-filter')) {
+    const gryffindor = dataList.filter(item => item.house === "Gryffindor")
+    console.log("gryffindor filter")
+    cardsOnDom(gryffindor)
+  }
+  if(event.target.id.includes('ravenclaw-filter')) {
+    const ravenclaw = dataList.filter(item => item.house === "Ravenclaw")
+    cardsOnDom(ravenclaw)
+  }
+  if(event.target.id.includes('hufflepuff-filter')) {
+    const hufflepuff = dataList.filter(item => item.house === "Hufflepuff")
+    cardsOnDom(hufflepuff)
+  }
+  if(event.target.id.includes('slytherin-filter')) {
+    const slytherin = dataList.filter(item => item.house === "Slytherin")
+    cardsOnDom(slytherin)
+  }
+}
+
+document.querySelector(".filter-container").addEventListener("click", houseFilter)
 //const startApp = () => {
   //welcomeCardOnDom()
 //}
